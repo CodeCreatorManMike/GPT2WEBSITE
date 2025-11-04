26c98f08d7ddcb4a4757921a1c5764b6ce1a931e
CodeCreatorManMike <michaeljonesincorporated@gmail.com>
2025-11-04 13:16:04 +0000

    Delete Card.js

--- Card.js
+++
@@ -1,8 +0,0 @@
-export function Card({ className = "", children }) {
-  return (
-    <div className={`rounded-2xl bg-white shadow-sm ring-1 ring-slate-100 ${className}`}>{children}</div>
-  );
-}
-export function CardContent({ className = "", children }) {
-  return <div className={`p-6 ${className}`}>{children}</div>;
-}
